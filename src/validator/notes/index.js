079a4fab9308f8e446c56e0aa314bbd00638bac7
ramahisyam <ramahisyam1@gmail.com>
2025-01-21 21:59:37 +0700

    Menggunakan NoteValidator pada Plugin Note

---
+++ src/validator/notes/index.js
@@ -0,0 +1,12 @@
+import { NotePayloadSchema } from './schema';
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new Error(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
